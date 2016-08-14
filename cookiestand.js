928faa3efe60a1ede91d5ac53f4ab1d9a98990cb
Lee Zamastil <l.zamastil@gmail.com>
2016-08-13 21:56:20 -0700

    Table headings work with start time and length of day variables

--- cookiestand.js
+++ cookiestand.js
@@ -5,20 +5,20 @@ var meridiem;                 // initializes am and pm string variable
 var totalSoldPerShop;         // declares variable for use within object contstructor
 
 // Count time on a 12-hour clock and add 'am' or 'pm' as appropriate
-// function getTimes() {
-//    if (i + openingHour < 12) {
-//       meridiem = 'am: ';
-//       civilianTime = i + openingHour;
-//    } else if (i + openingHour > 12) {
-//       meridiem = 'pm: ';
-//       civilianTime = i + openingHour - 12;
-//    } else {
-//       meridiem = 'pm: ';
-//       civilianTime = i + openingHour;
-//    }
-// }
+function getTimes() {
+   if (i + openingHour < 12) {
+      meridiem = 'am';
+      civilianTime = i + openingHour;
+   } else if (i + openingHour > 12) {
+      meridiem = 'pm';
+      civilianTime = i + openingHour - 12;
+   } else {
+      meridiem = 'pm';
+      civilianTime = i + openingHour;
+   }
+}
 
-
+// Object contstructor
 var Shop = function(shopName, minCust, maxCust, avgCookiePerCust) {
   this.shopName = shopName;
   this.minCust = minCust;
@@ -32,6 +32,9 @@ var Shop = function(shopName, minCust, maxCust, avgCookiePerCust) {
   };
   this.addCookiesPerHour = function() {
     var shopRow = document.createElement("tr");         // shopRow holds <tr> element
+    var shopCell = document.createElement("td");        // shopCell holds <td> element
+    shopCell.innerText = this.shopName;                 // sticks the shopName into the <td> element
+    shopRow.appendChild(shopCell);                      // <td> with shopName appended to the table row <tr>
     var totalSoldPerShop = 0;                           // reset total counter to 0 for each store loop
     for (var index = 0; index < hoursToCalculate; index++) {
       var cookieCell = document.createElement("td");    // cookieCell holds <td> element
@@ -40,12 +43,11 @@ var Shop = function(shopName, minCust, maxCust, avgCookiePerCust) {
       shopRow.appendChild(cookieCell);                  // <td> with rnd# appended to the table row <tr>
       totalSoldPerShop += cookiePerHourNumber;          // store total is incremented
     };
-    // console.log(totalSoldPerShop);
     cookieCell = document.createElement("td");          // cookieCell cleared to hold just <td> element again
     cookieCell.innerText = totalSoldPerShop;            // sticks the store total in <td> element
     shopRow.appendChild(cookieCell);                    // <td> with store total appended to the table row <tr>
     var table = document.getElementById("shops");       // variable table holds reference to HTML <table>
-    table.appendChild(shopRow);                         // table row <tr> appended to HTML <table>
+    table.appendChild(shopRow);                         // table row <tr> with all <td>'s appended to HTML <table>
   }  // closes the addCookiesPerHour function
 }
 
@@ -58,6 +60,43 @@ var shops = [
   new Shop("Pearl District", 3, 24, 2.6),
 ];
 
+
+// Add table header "Cookie Stand Locations"
+var table = document.getElementById("shops");
+var tableHeader = document.createElement("tr");
+var tableHeaderCell = document.createElement("th");
+tableHeaderCell.setAttribute("colspan", "9");
+tableHeaderCell.innerText = "Cookie Stand Locations";
+tableHeader.appendChild(tableHeaderCell);
+table.appendChild(tableHeader);
+
+
+// Add column headers
+tableHeader = document.createElement("tr");
+
+tableHeaderCell = document.createElement("th");
+tableHeaderCell.innerText = "Location";
+tableHeader.appendChild(tableHeaderCell);
+
+for (var i = 0; i < hoursToCalculate; i++) {
+  getTimes();  // required function call to get data for civilianTime and meridiem variables
+  tableHeaderCell = document.createElement("th");
+  tableHeaderCell.innerText = civilianTime + meridiem;
+  tableHeader.appendChild(tableHeaderCell);
+}
+// tableHeaderCell = document.createElement("th");
+// tableHeaderCell.innerText = "E-Mail";
+// tableHeader.appendChild(tableHeaderCell);
+
+tableHeaderCell = document.createElement("th");
+tableHeaderCell.innerText = "Total";
+tableHeader.appendChild(tableHeaderCell);
+
+table.appendChild(tableHeader);
+
+
+
+// Add cookie counts
 for (var index = 0; index < shops.length; index++) {
   var cookieShop = shops[index];
   cookieShop.addCookiesPerHour();
